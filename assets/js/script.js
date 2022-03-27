5887ecea243fe62e3a5c758d5d313c8e2009d580
Amir <amir.hackett@gmail.com>
2022-03-27 18:44:49 -0400

    added new variables to JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,48 +1,9 @@
-var cityFormEl = document.querySelector("#city-search-form")
-var weatherContainer = document.querySelector("#current-weather-container")
+var cities = [];
 
-function cityFetch(){
-   var apiCity = "https://api.openweathermap.org/data/2.5/weather?q=orlando&appid=b82992c00ffe5866266f560867525932"
-   fetch(apiCity)
-   .then(function(response){
-       if(response.ok){
-           response.json().then(function(data){
-            var lat = data.coord.lat
-            var lon = data.coord.lon   
-            displayCity(lat, lon)
-           })
-       }
-   })
-}
-
-function displayCity(lat, lon){
-    var apiLatLong = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude={part}&appid=b82992c00ffe5866266f560867525932"
-        fetch(apiLatLong)
-        .then(function(response){
-            //console.log(response)
-            if(response.ok){
-                response.json().then(function(response){
-                    for(var i = 0; i < response.daily.length -3; i++){
-                        var dateCard = document.createElement("div")
-                        dateCard.textContent = moment.unix(response.daily[i].dt).format("MM/DD/YYYY")
-                        var condCard = document.createElement("img")
-                        condCard.setAttribute("src", `https://openweathermap.org/img/w/${response.daily[i].weather[0].icon}.png`)
-                        var tempCard = document.createElement("div")
-                        tempCard.textContent 
-                        var windCard = document.createElement("div")
-                        windCard.textContent
-                        var humidCard = document.createElement("div")
-                        humidCard.textContent
-                        var card = document.createElement("div")
-                        card.setAttribute("class", "card")
-                        card.append(dateCard, condCard, tempCard, windCard, humidCard)
-                        weatherContainer.append(card)
-                        console.log(response)
-                    }
-                })
-            }
-        })
-    }
-
-
-cityFetch()
+var cityFormEl=document.querySelector("#city-search-form");
+var cityInputEl=document.querySelector("#city");
+var weatherContainerEl=document.querySelector("#current-weather-container");
+var citySearchInputEl = document.querySelector("#searched-city");
+var forecastTitle = document.querySelector("#forecast");
+var forecastContainerEl = document.querySelector("#fiveday-container");
+var pastSearchButtonEl = document.querySelector("#past-search-button");
