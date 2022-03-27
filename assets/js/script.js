eaab6d90834301cb7720cc84d09c3ff9a632ce89
Amir <amir.hackett@gmail.com>
2022-03-27 19:17:11 -0400

    added functions to call API:

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,42 @@
 var cities = [];
-
 var cityFormEl=document.querySelector("#city-search-form");
 var cityInputEl=document.querySelector("#city");
 var weatherContainerEl=document.querySelector("#current-weather-container");
 var citySearchInputEl = document.querySelector("#searched-city");
 var forecastTitle = document.querySelector("#forecast");
-var forecastContainerEl = document.querySelector("#fiveday-container");
+var forecastContainerEl = document.querySelector("#five-day-container");
 var pastSearchButtonEl = document.querySelector("#past-search-button");
+
+var getCityWeather = function(city){
+    var apiKey = "b82992c00ffe5866266f560867525932"
+    var apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+
+    fetch(apiURL)
+    .then(function(response){
+        response.json().then(function(data){
+            displayWeather(data, city);
+        });
+    });
+};
+
+var formSumbitHandler = function(event){
+    event.preventDefault();
+    var city = cityInputEl.value.trim();
+    if(city){
+        getCityWeather(city);
+        get5Day(city);
+        cities.unshift({city});
+        cityInputEl.value = "";
+    } else{
+        alert("Please enter a City");
+    }
+    saveSearch();
+    pastSearch(city);
+}
+
+var saveSearch = function(){
+    localStorage.setItem("cities", JSON.stringify(cities));
+};
+
+
+cityFormEl.addEventListener("submit", formSumbitHandler);
