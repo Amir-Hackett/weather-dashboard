e8b7f156304f3711ab1de8f1b331132f56b78032
Amir <amir.hackett@gmail.com>
2022-03-27 20:12:32 -0400

    added function to display weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ var forecastTitle = document.querySelector("#forecast");
 var forecastContainerEl = document.querySelector("#five-day-container");
 var pastSearchButtonEl = document.querySelector("#past-search-button");
 
-var getCityWeather = function(city){
+function getCityWeather(city){
     var apiKey = "b82992c00ffe5866266f560867525932"
     var apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
 
@@ -19,7 +19,7 @@ var getCityWeather = function(city){
     });
 };
 
-var formSumbitHandler = function(event){
+function formSumbitHandler(event){
     event.preventDefault();
     var city = cityInputEl.value.trim();
     if(city){
@@ -34,9 +34,53 @@ var formSumbitHandler = function(event){
     pastSearch(city);
 }
 
-var saveSearch = function(){
+function saveSearch(){
     localStorage.setItem("cities", JSON.stringify(cities));
 };
 
+function displayWeather(weather, searchCity){
+    //clear old content
+    weatherContainerEl.textContent= "";  
+    citySearchInputEl.textContent=searchCity;
+ 
+    //create date element
+    var currentDate = document.createElement("span")
+    currentDate.textContent=" (" + moment(weather.dt.value).format("MMM D, YYYY") + ") ";
+    citySearchInputEl.appendChild(currentDate);
+ 
+    //create an image element
+    var weatherIcon = document.createElement("img")
+    weatherIcon.setAttribute("src", `https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`);
+    citySearchInputEl.appendChild(weatherIcon);
+ 
+    //create a span element to hold temperature data
+    var temperatureEl = document.createElement("span");
+    temperatureEl.textContent = "Temperature: " + weather.main.temp + " °F";
+    temperatureEl.classList = "list-group-item"
+   
+    //create a span element to hold Humidity data
+    var humidityEl = document.createElement("span");
+    humidityEl.textContent = "Humidity: " + weather.main.humidity + " %";
+    humidityEl.classList = "list-group-item"
+ 
+    //create a span element to hold Wind data
+    var windSpeedEl = document.createElement("span");
+    windSpeedEl.textContent = "Wind Speed: " + weather.wind.speed + " MPH";
+    windSpeedEl.classList = "list-group-item"
+ 
+    //append to container
+    weatherContainerEl.appendChild(temperatureEl);
+ 
+    //append to container
+    weatherContainerEl.appendChild(humidityEl);
+ 
+    //append to container
+    weatherContainerEl.appendChild(windSpeedEl);
+ 
+    var lat = weather.coord.lat;
+    var lon = weather.coord.lon;
+    getUvIndex(lat,lon)
+ }
 
-cityFormEl.addEventListener("submit", formSumbitHandler);
+cityFormEl.addEventListener("submit", formSumbitHandler)
+pastSearchButtonEl.addEventListener("click", pastSearchHandler)
